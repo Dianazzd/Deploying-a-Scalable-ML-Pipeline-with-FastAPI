e097cd448558c01ea85dc9ac25425e7c2662ca38
Dianazzd <zandongzhang77@gmail.com>
2025-03-05 19:48:20 -0500

    complete unit test

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,58 @@
 import pytest
-# TODO: add necessary import
+import pandas as pd
+import numpy as np
+import sklearn
+import os
+from sklearn.model_selection import train_test_split
+from ml.model import train_model
+from ml.data import process_data
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+
+@pytest.fixture
+def data():
+    data_path = os.path.join(os.getcwd(), "data", "census.csv")
+    df = pd.read_csv(data_path)
+    return df
+
+
+def test_train_model(data):
     """
-    # add description for the first test
+    # check the function uses a RandomForest Classifier
     """
-    # Your code here
-    pass
+    train, test = train_test_split(data, test_size=0.2)
+    cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+    train, 
+    categorical_features=cat_features, 
+    label="salary", 
+    training=True
+    )
+    model = train_model(X_train, y_train)
+    assert type(model) == sklearn.ensemble._forest.RandomForestClassifier
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_dataset_cols(data):
     """
-    # add description for the second test
+    # check number of columns in the dataset
     """
-    # Your code here
-    pass
+    assert data.shape[1] == 15
+    
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_dataset_shape(data):
     """
-    # add description for the third test
+    # make sure the dataset has no null values
     """
-    # Your code here
-    pass
+    assert data.shape == data.dropna().shape
+    
